0e2e7e999cd7308e53f94f3c7b5c069944086614
Tanmay Kadam <tanmaykadam.scoe.it@gmail.com>
2025-07-27 22:40:25 +0530

    added actiontype.js in redux

---
+++ client/src/redux/actionTypes.js
@@ -0,0 +1,7 @@
+export const SET_USER = "SET_USER";
+export const LOGOUT = "LOGOUT";
+export const SET_LOADING = "SET_LOADING";
+export const UPDATE_USER = "UPDATE_USER";
+export const SAVE_PREFERENCES = "SAVE_PREFERENCES"; // ✅ Add this if missing
+export const SET_ERROR = "SET_ERROR";
+export const SET_MATCHES = 'SET_MATCHES';
